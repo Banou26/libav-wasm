aa99c44d1e5be51334aaadbd23014c1a12e90dc3
Banou <me@banou.dev>
2023-09-02 20:04:01 +0200

    fix: 🚧push prevent seeking code, to fix seeking later

--- src/test.ts
+++ src/test.ts
@@ -464,11 +464,53 @@ fetch(VIDEO_URL, { headers: { Range: `bytes=0-1` } })
     await processNeededBufferRange()
     await updateBufferedRanges()
 
-    video.addEventListener('seeking', () => {
-      const isPlaying = !video.paused
-      video.pause()
-      seek(video.currentTime, isPlaying)
-    })
+
+
+
+
+
+
+
+
+    // THIS PREVENTS SEEKING EVEN WITH NATIVE SEEKING
+    // var supposedCurrentTime = 0;
+    // video.addEventListener('timeupdate', function() {
+    //   if (!video.seeking) {
+    //         supposedCurrentTime = video.currentTime;
+    //   }
+    // });
+    // // prevent user from seeking
+    // video.addEventListener('seeking', function() {
+    //   // guard agains infinite recursion:
+    //   // user seeks, seeking is fired, currentTime is modified, seeking is fired, current time is modified, ....
+    //   var delta = video.currentTime - supposedCurrentTime;
+    //   if (Math.abs(delta) > 0.01) {
+    //     console.log("Seeking is disabled");
+    //     video.currentTime = supposedCurrentTime;
+    //   }
+    // });
+    // // delete the following event handler if rewind is not required
+    // video.addEventListener('ended', function() {
+    //   // reset state in order to allow for rewind
+    //     supposedCurrentTime = 0;
+    // });
+
+
+
+
+
+
+
+
+
+
+
+    // video.addEventListener('seeking', (event) => {
+    //   event.preventDefault()
+    //   // const isPlaying = !video.paused
+    //   // video.pause()
+    //   // seek(video.currentTime, isPlaying)
+    // })
 
     const timeUpdateWork = queuedDebounceWithLastCall(500, async (time: number) => {
       await processNeededBufferRange(time + POST_SEEK_NEEDED_BUFFERS_IN_SECONDS)
